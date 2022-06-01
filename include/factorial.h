c81cb2ddebc9ae1fbf05586fccff95b94635a2b4
Devin <18190816904@163.com>
2022-06-01 15:15:01 +0800

    头文件

---
+++ include/factorial.h
@@ -0,0 +1,12 @@
+//
+// Created by 周昊 on 2022/6/1.
+//
+
+#ifndef C_DEMO_FACTORIAL_H
+#define C_DEMO_FACTORIAL_H
+
+unsigned int Factorial(unsigned int n);
+
+unsigned int FactorialByIteration(unsigned int n);
+
+#endif //C_DEMO_FACTORIAL_H
---
+++ my_include_file.c
@@ -0,0 +1,10 @@
+//
+// Created by 周昊 on 2022/6/1.
+//
+#include <stdio.h>
+#include <factorial.h>
+
+int main() {
+    printf("6! = %d\n", Factorial(6));
+    return 0;
+}
