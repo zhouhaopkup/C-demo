892d4b0749850d636366a31a582d585080335ecb
Devin <18190816904@163.com>
2022-06-05 17:33:01 +0800

    字符串

---
+++ string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "io_utils.h"
+
+int main() {
+    char string[] = "Hello World";
+
+    for(int i = 0; i < 11; ++i) {
+        PRINT_CHAR(string[i]);
+    }
+
+    // NULL \0
+    PRINTLNF("%s", string);
+
+    char string_zh = "你好, 中国";
+
+    wchar_t ws[] = L"你好, 中国";
+
+    return 0;
+}
+
