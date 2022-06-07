d4de68f840c02391394781ee9e0f171f271557d5
Devin <18190816904@163.com>
2022-06-07 09:28:58 +0800

    获取系统时间

---
+++ get_sys_time.c
@@ -0,0 +1,15 @@
+#include "io_utils.h"
+#include <time.h>
+#include "time_utils.h"
+
+int main() {
+    time_t current_time;
+    time(&current_time);
+    PRINT_LONG(current_time);
+
+    current_time = time(NULL);
+    PRINT_LONG(current_time);
+
+    PRINT_LLONG(TimeInMillisecond());
+    return 0;
+}
